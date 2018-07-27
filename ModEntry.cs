4f79964026a93c8bc79f6577d36e867af7821ecd
Jesse Vickery <jesse_vickery@outlook.com>
2018-07-27 14:35:45 -0400

    Add files via upload

---
+++ ModEntry.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+using StardewModdingAPI;
+using StardewValley.Buildings;
+using StardewValley.Locations;
+using StardewValley.Menus;
+using StardewValley;
+
+using xTile;
+
+using SObject = StardewValley.Object;
+using StardewModdingAPI.Events;
+using System.Reflection;
+
+namespace Sauvignon_in_Stardew
+{
+    class ModEntry : Mod, IAssetLoader
+    {
+        /*
+         * FIELDS
+         * 
+         */         
+        public static IModHelper helper;
+        public static IMonitor monitor;
+
+        public Texture2D Winery_outdoors;
+        public Map Winery_indoors;
+
+        private readonly Dictionary<string, string> dataForBlueprint = new Dictionary<string, string>() { ["Winery"]= "709 200 330 100 390 100/11/6/5/5/-1/-1/Winery/Winery/Kegs and Casks inside work 30% faster and display time remaining./Buildings/none/96/96/20/null/Farm/20000/false" };
+        /*
+         * END FIELDS
+         * 
+         */ 
+
+            
+
+        /*
+         * ENTRY
+         * 
+         */ 
+        public override void Entry(IModHelper helper)
+        {
+            ModEntry.monitor = this.Monitor;
+            ModEntry.helper = helper;
+
+            Winery_outdoors = helper.Content.Load<Texture2D>("assets/Winery_outside.png", ContentSource.ModFolder);
+            Winery_indoors = helper.Content.Load<Map>("assets/Winery.tbin", ContentSource.ModFolder);
+
+            MenuEvents.MenuChanged += MenuEvents_MenuChanged;
+        }
+        /*
+        * END ENTRY
+        * 
+        */
+
+        private void MenuEvents_MenuChanged(object sender, EventArgsClickableMenuChanged e)
+        {
+            if (Game1.activeClickableMenu is CarpenterMenu carpenterMenu)
+            {
+                if (!IsMagical(carpenterMenu) && !HasBluePrint(carpenterMenu))
+                {
+                    BluePrint wineryBluePrint = new BluePrint("Slime Hutch")
+                    {
+                        name = "Winery",
+                        displayName = "Winery",
+                        description = "Kegs and Casks inside work 30% faster and display time remaining.",
+                        moneyRequired = 0 //20000
+                    };
+                    wineryBluePrint.itemsRequired.Clear();
+                    wineryBluePrint.itemsRequired.Add(709, 0);//200
+                    wineryBluePrint.itemsRequired.Add(330, 0);//100
+                    wineryBluePrint.itemsRequired.Add(390, 0);//100
+
+                    SetBluePrintField(wineryBluePrint, "textureName", "Buildings\\Winery");
+                    SetBluePrintField(wineryBluePrint, "texture", Game1.content.Load<Texture2D>(wineryBluePrint.textureName));
+
+                    GetBluePrints(carpenterMenu).Add(wineryBluePrint);
+                }
+            }
+        }
+
+        private static bool IsMagical(CarpenterMenu carpenterMenu)
+        {
+            return (bool)typeof(CarpenterMenu).GetField("magicalConstruction", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public).GetValue(carpenterMenu);
+        }
+
+        private static bool HasBluePrint(CarpenterMenu carpenterMenu)
+        {
+            return GetBluePrints(carpenterMenu).Exists(bluePrint => bluePrint.name == "Winery");
+        }
+
+        private static List<BluePrint> GetBluePrints(CarpenterMenu carpenterMenu)
+        {
+            return (List<BluePrint>)typeof(CarpenterMenu).GetField("blueprints", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public).GetValue(carpenterMenu);
+        }
+
+        private static void SetBluePrintField(BluePrint bluePrint, string field, object value)
+        {
+            typeof(BluePrint).GetField(field, BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public).SetValue(bluePrint, value);
+        }
+
+
+
+        /*
+         * ASSET LOADER
+         * 
+         */
+        public bool CanLoad<T>(IAssetInfo asset)
+        {
+            if (asset.AssetNameEquals("Buildings\\Winery"))
+            {
+                return true;
+            }
+            else if (asset.AssetNameEquals("Maps/Winery"))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public T Load<T>(IAssetInfo asset)
+        {
+            if (asset.AssetNameEquals("Buildings\\Winery"))
+            {
+                return (T)(object)Winery_outdoors;
+            }
+            else if (asset.AssetNameEquals("Maps/Winery"))
+            {
+                return (T)(object)Winery_indoors;
+            }
+            return (T)(object)null;
+        }
+        /*
+         * END ASSET LOADER
+         * 
+         */ 
+    }
+}
